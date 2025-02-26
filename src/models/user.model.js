86286776a9caeb57eea4fd51466c54ac3b8c1d88
yogesh8722 <yogeshdhanera.in>
2025-02-26 16:43:26 +0530

    video and user model

---
+++ src/models/user.model.js
@@ -0,0 +1,86 @@
+import mongoose,{Schema} from "mongoose";
+import bcrypt from 'bcrypt';
+import jsonwebtoken from 'jsonwebtoken';
+
+const userSchema=new Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true,
+        index:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true
+    },
+    fullname:{
+        type:String,
+        required:true,
+        trim:true,
+        index:true
+    },
+    avatar:{
+        type:String, //cloudnary
+        required:true
+    },
+    coverImage:{
+        type:String //cloudnary
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    watchHistory:[
+        {
+           type:Schema.Types.ObjectId,
+           ref:"Video"
+        }
+    ],
+    refreshToken:{
+        type:String
+    }
+},{timestamps:true})
+
+userSchema.pre("save",async function(next) {
+    if(!this.isModified("password")) return next();
+
+    this.password=bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect=async function (password){
+    return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken=async function (){
+   return await jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            username:this.username,
+            fullname:this.fullname
+
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken=async function (){
+    return await jwt.sign({
+        _id:this._id
+    }),
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+}
+
+export const User=mongoose.model("User",userSchema)
